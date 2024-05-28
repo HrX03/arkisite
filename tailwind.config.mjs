decca50909c2f9b0bd043ec68f06384e01b27ffb
Davide Bianco <dn.bianco03@gmail.com>
2024-05-28 22:59:29 +0200

    Fix compilation pog

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -22,28 +22,6 @@ export default {
         "foreground": "var(--fg-color)",
         "secondary": "var(--snd-color)",
       },
-      keyframes: {
-        "marquee-gradient": {
-          from: {
-            transform: 'translateX(0%);'
-          },
-          to: {
-            transform: 'translateX(-672px);'
-          },
-        },
-        "marquee-gradient-re": {
-          from: {
-            transform: 'translateX(-672px);'
-          },
-          to: {
-            transform: 'translateX(0%);'
-          },
-        },
-      },
-      animation: {
-        "marquee-gradient": "marquee-gradient 5s infinite linear;",
-        "marquee-gradient-re": "marquee-gradient-re 5s infinite linear;",
-      }
     },
     /* Most of the time we customize the font-sizes,
      so we added the Tailwind default values here for
